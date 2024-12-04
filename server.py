4f2252db939149d84449ce2f8019321e182b6aec
Enkhbayar <ganboldenkhbayar0823@gmail.com>
2024-12-04 18:30:48 +0800

    Server-iin code-iig AI-iin code-ruu zuuv

--- server.py
+++ server.py
@@ -1,39 +1,43 @@
-from flask import Flask, request, jsonify
-from flask_ngrok import run_with_ngrok
-from flask_cors import CORS
+##### AI байгаа хэсэг сервер-ийг ажлуулахын тулд доорх код-ийг copy_of_algo_biy_daalt2.py -руу зөөв #####
 
-app = Flask(__name__)
-CORS(app, resources={r"/process": {"origins": "*"}})
 
-@app.route('/process', methods=['POST'])
-def process():
-    data = request.get_json()
+# from flask import Flask, request, jsonify
+# from flask_ngrok import run_with_ngrok
+# from flask_cors import CORS
 
-    if 'test' not in data:
-        return jsonify({"error": "Missing 'test' parameter"}), 400
 
-    test = data['test']
+# app = Flask(__name__)
+# CORS(app, resources={r"/process": {"origins": "*"}})
 
-    try:
-        zuw_ug, buruu_ug, ugnuud = ug_shalgah(test)
-        stems = undes_ug(ugnuud)
-        final_sentence = undes_ug_sentence(stems)
-        suggestions = buruu_ug_sanal(buruu_ug)
+# @app.route('/process', methods=['POST'])
+# def process():
+#     data = request.get_json()
 
-        sedev = predict_news(test)
+#     if 'test' not in data:
+#         return jsonify({"error": "Missing 'test' parameter"}), 400
 
-        response = {
-            "zuw_ug": zuw_ug,
-            "buruu_ug": buruu_ug,
-            "final_sentence": final_sentence,
-            "suggestions": suggestions,
-            "sedev":sedev
-        }
+#     test = data['test']
 
-        return jsonify(response)
+#     try:
+#         zuw_ug, buruu_ug, ugnuud = ug_shalgah(test)
+#         stems = undes_ug(ugnuud)
+#         final_sentence = undes_ug_sentence(stems)
+#         suggestions = buruu_ug_sanal(buruu_ug)
 
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+#         sedev = predict_news(test)
 
-run_with_ngrok(app)
-app.run()
+#         response = {
+#             "zuw_ug": zuw_ug,
+#             "buruu_ug": buruu_ug,
+#             "final_sentence": final_sentence,
+#             "suggestions": suggestions,
+#             "sedev":sedev
+#         }
+
+#         return jsonify(response)
+
+#     except Exception as e:
+#         return jsonify({"error": str(e)}), 500
+
+# run_with_ngrok(app)
+# app.run()
